88c25f7d0835e55632288606af87d2e1d7402a57
Malav Bateriwala <b.malav93@gmail.com>
2023-06-25 23:32:04 +0100

    Create upload_artifact.py

---
+++ upload_artifact.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import argparse
+from mb.utils import logging
+import pathlib
+import wandb
+
+logger = logging.logger
+
+
+def go(args):
+
+    logger.info("Creating run in project ")
+    run = wandb.init(project="name", job_type="use_file")
+
+    logger.info("Getting artifact")
+    artifact = run.use_artifact(args.artifact_name)
+
+    logger.info("Artifact content:")
+    filepath = artifact.file()
+
+    with open(filepath, "r") as fp:
+        content = fp.read()
+
+    print(content)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Use an artifact from W&B", fromfile_prefix_chars="@"
+    )
+
+    parser.add_argument(
+        "--artifact_name", type=str, help="Name and version of W&B artifact", required=True
+    )
+
+    args = parser.parse_args()
+
+    go(args)
